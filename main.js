4f0c914ea9194b23d52bf80da397256f55215e97
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-03 15:50:41 +0200

    primitive and reference types

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const another = new Circle(1); */
 
 
 // Functions are objects
-function Circle(radius){
+/* function Circle(radius){
     this.radius = radius;
     this.draw = function(){
         console.log('Draw');
@@ -47,4 +47,34 @@ function Circle(radius){
 Circle.call({}, 1);
 Circle.apply({}, [1,2,3]);
 
-const another = new Circle(1);
+const another = new Circle(1); */
+
+
+
+/* Value types/Primitives V/S Reference types */
+
+//Value types
+let x = 10;
+let y = x;
+x= 20;
+
+//Reference types
+let x1 = {value: 10};
+let y1 = x;
+x1.value= 20;
+
+
+// Examples
+let number = 10;
+function increase(number){
+    number++;
+}
+increase(number);
+console.log(number);
+
+let obj = {value: 10 };
+function increase(obj){
+    obj.value++;
+}
+increase(obj);
+console.log(obj);
